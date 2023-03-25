0e0116a41fe23f258d257493f69d38881d3519e8
maheshpoojaryneu <poojary.m@northeastern.edu>
2023-03-25 01:50:49 -0400

    added table cleanup script

---
+++ step_0_table_cleanup.sql
@@ -0,0 +1,30 @@
+set serveroutput on
+declare
+    v_table_exists varchar(1) := 'Y';
+    v_sql varchar(2000);
+begin
+   dbms_output.put_line('Start schema cleanup');
+   for i in (select table_name from user_tables
+   )
+   loop
+   dbms_output.put_line('....Drop table '||i.table_name);
+   begin
+       select 'Y' into v_table_exists
+       from USER_TABLES
+       where TABLE_NAME=i.table_name;
+
+       v_sql := 'drop table '||i.table_name;
+       execute immediate v_sql;
+       dbms_output.put_line('........Table '||i.table_name||' dropped successfully');
+       
+   exception
+       when no_data_found then
+           dbms_output.put_line('........Table already dropped');
+   end;
+   end loop;
+   dbms_output.put_line('Schema cleanup successfully completed');
+exception
+   when others then
+      dbms_output.put_line('Failed to execute code:'||sqlerrm);
+end;
+/
