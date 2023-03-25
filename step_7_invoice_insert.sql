3dd2c10a571f17085758d32460d5e09c9fa776ee
maheshpoojaryneu <poojary.m@northeastern.edu>
2023-03-24 23:12:02 -0400

    updated invoice insert script and added voucher insert script

--- step_7_invoice_insert.sql
+++ step_7_invoice_insert.sql
@@ -1,149 +1,36 @@
+SET DEFINE OFF
 
-Error starting at line : 3 in command -
-INSERT INTO INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
-VALUES (1, 1000, to_date('03-Jan-23', 'DD-MON-RR'))
-Error at Command Line : 3 Column : 13
-Error report -
-SQL Error: ORA-00942: table or view does not exist
-00942. 00000 -  "table or view does not exist"
-*Cause:    
-*Action:
+INSERT INTO ADMIN.INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
+VALUES (1, 1000, to_date('03-Jan-23', 'DD-MON-RR'));
 
-Error starting at line : 6 in command -
-INSERT INTO INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
-VALUES (2, 5000, to_date('06-Jan-23', 'DD-MON-RR'))
-Error at Command Line : 6 Column : 13
-Error report -
-SQL Error: ORA-00942: table or view does not exist
-00942. 00000 -  "table or view does not exist"
-*Cause:    
-*Action:
+INSERT INTO ADMIN.INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
+VALUES (2, 5000, to_date('06-Jan-23', 'DD-MON-RR'));
 
-Error starting at line : 9 in command -
-INSERT INTO INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
-VALUES (3, 1000, to_date('08-Jan-23', 'DD-MON-RR'))
-Error at Command Line : 9 Column : 13
-Error report -
-SQL Error: ORA-00942: table or view does not exist
-00942. 00000 -  "table or view does not exist"
-*Cause:    
-*Action:
+INSERT INTO ADMIN.INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
+VALUES (3, 1000, to_date('08-Jan-23', 'DD-MON-RR'));
 
-Error starting at line : 12 in command -
-INSERT INTO INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
-VALUES (4, 1000, to_date('13-Jan-23', 'DD-MON-RR'))
-Error at Command Line : 12 Column : 13
-Error report -
-SQL Error: ORA-00942: table or view does not exist
-00942. 00000 -  "table or view does not exist"
-*Cause:    
-*Action:
+INSERT INTO ADMIN.INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
+VALUES (4, 1000, to_date('13-Jan-23', 'DD-MON-RR'));
 
-Error starting at line : 15 in command -
-INSERT INTO INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
-VALUES (5, 2000, to_date('03-Feb-23', 'DD-MON-RR'))
-Error at Command Line : 15 Column : 13
-Error report -
-SQL Error: ORA-00942: table or view does not exist
-00942. 00000 -  "table or view does not exist"
-*Cause:    
-*Action:
+INSERT INTO ADMIN.INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
+VALUES (5, 2000, to_date('03-Feb-23', 'DD-MON-RR'));
 
-Error starting at line : 18 in command -
-INSERT INTO INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
-VALUES (6, 1750, to_date('03-Feb-23', 'DD-MON-RR'))
-Error at Command Line : 18 Column : 13
-Error report -
-SQL Error: ORA-00942: table or view does not exist
-00942. 00000 -  "table or view does not exist"
-*Cause:    
-*Action:
+INSERT INTO ADMIN.INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
+VALUES (6, 1750, to_date('03-Feb-23', 'DD-MON-RR'));
 
-Error starting at line : 21 in command -
-INSERT INTO INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
-VALUES (7, 1500, to_date('05-Feb-23', 'DD-MON-RR'))
-Error at Command Line : 21 Column : 13
-Error report -
-SQL Error: ORA-00942: table or view does not exist
-00942. 00000 -  "table or view does not exist"
-*Cause:    
-*Action:
+INSERT INTO ADMIN.INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
+VALUES (7, 1500, to_date('05-Feb-23', 'DD-MON-RR'));
 
-Error starting at line : 24 in command -
-INSERT INTO INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
-VALUES (8, 250, to_date('11-Feb-23', 'DD-MON-RR'))
-Error at Command Line : 24 Column : 13
-Error report -
-SQL Error: ORA-00942: table or view does not exist
-00942. 00000 -  "table or view does not exist"
-*Cause:    
-*Action:
+INSERT INTO ADMIN.INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
+VALUES (8, 250, to_date('11-Feb-23', 'DD-MON-RR'));
 
-Error starting at line : 27 in command -
-INSERT INTO INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
-VALUES (9, 250, to_date('11-Feb-23', 'DD-MON-RR'))
-Error at Command Line : 27 Column : 13
-Error report -
-SQL Error: ORA-00942: table or view does not exist
-00942. 00000 -  "table or view does not exist"
-*Cause:    
-*Action:
+INSERT INTO ADMIN.INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
+VALUES (9, 250, to_date('11-Feb-23', 'DD-MON-RR'));
 
-Error starting at line : 30 in command -
-INSERT INTO INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
-VALUES (10, 500, to_date('13-Feb-23', 'DD-MON-RR'))
-Error at Command Line : 30 Column : 13
-Error report -
-SQL Error: ORA-00942: table or view does not exist
-00942. 00000 -  "table or view does not exist"
-*Cause:    
-*Action:
+INSERT INTO ADMIN.INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
+VALUES (10, 500, to_date('13-Feb-23', 'DD-MON-RR'));
 
-Error starting at line : 33 in command -
-INSERT INTO INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
-VALUES (11, 800, to_date('16-Feb-23', 'DD-MON-RR'))
-Error at Command Line : 33 Column : 13
-Error report -
-SQL Error: ORA-00942: table or view does not exist
-00942. 00000 -  "table or view does not exist"
-*Cause:    
-*Action:
+INSERT INTO ADMIN.INVOICE (PO_ID, GROSS_AMT, CREATED_DATE) 
+VALUES (11, 800, to_date('16-Feb-23', 'DD-MON-RR'));
 
-Commit complete.
-
-
-1 row inserted.
-
-
-1 row inserted.
-
-
-1 row inserted.
-
-
-1 row inserted.
-
-
-1 row inserted.
-
-
-1 row inserted.
-
-
-1 row inserted.
-
-
-1 row inserted.
-
-
-1 row inserted.
-
-
-1 row inserted.
-
-
-1 row inserted.
-
-
-Commit complete.
-
+commit;
